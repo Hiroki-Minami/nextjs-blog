29bbcd0b5949612bdc00abf55a6a4678f8f03215
Hiroki Minami <hirokimnm6@gmail.com>
2022-08-29 14:56:06 -0700

    fixed message

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home({allPostsData}) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>I'm Hiroki Minami.</p>
+        <p>I am Hiroki Minami.</p>
         <p>
           (This is a sample website - you will be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
